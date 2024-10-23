60bcfe61313581d4ca5c7d8045c58bb8057072ae
MASIH1903 <144231041+MASIH1903@users.noreply.github.com>
2024-10-23 11:28:52 +0200

    Lavet 1 - 4 dao metoder til endpoints

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,7 +1,5 @@
 package app;
 
-import app.config.AppConfig;
-
 import java.io.IOException;
 import java.net.URISyntaxException;
 
@@ -9,15 +7,6 @@ public class Main
 {
     public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException
     {
-        AppConfig.startServer();
-
-//        EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory();
-//
-//        CountryService countryService = new CountryService();
-//        List<CountryDTO> countryDTOList = countryService.fetchAllData();
-//
-//        CountryDAO countryDAO = new CountryDAO(emf);
-//       countryDAO.saveCountriesToDb(countryDTOList);
-
+        //AppConfig.startServer();
     }
 }
--- src/main/java/app/daos/CountryDAO.java
+++ src/main/java/app/daos/CountryDAO.java
@@ -1,6 +1,5 @@
 package app.daos;
 
-import app.controllers.CountryController;
 import app.dtos.CountryDTO;
 import app.entities.Country;
 import jakarta.persistence.EntityManager;
@@ -10,30 +9,24 @@ import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
-public class CountryDAO implements IDAO<Country>
-{
+public class CountryDAO implements IDAO<Country> {
     private final EntityManagerFactory emf;
     private final Logger log = LoggerFactory.getLogger(CountryDAO.class);
 
-    public CountryDAO(EntityManagerFactory emf)
-    {
+    public CountryDAO(EntityManagerFactory emf) {
         this.emf = emf;
     }
 
     @Override
-    public Country getById(Long id)
-    {
-        try (EntityManager em = emf.createEntityManager())
-        {
+    public Country getById(Long id) {
+        try (EntityManager em = emf.createEntityManager()) {
             return em.find(Country.class, id);
         }
     }
 
     @Override
-    public List<Country> getAll()
-    {
-        try (EntityManager em = emf.createEntityManager())
-        {
+    public List<Country> getAll() {
+        try (EntityManager em = emf.createEntityManager()) {
             return em.createQuery("SELECT c FROM Country c", Country.class).getResultList();
         }
     }
@@ -68,10 +61,8 @@ public class CountryDAO implements IDAO<Country>
 
 
     @Override
-    public void update(Country country)
-    {
-        try (EntityManager em = emf.createEntityManager())
-        {
+    public void update(Country country) {
+        try (EntityManager em = emf.createEntityManager()) {
             em.getTransaction().begin();
             em.merge(country);
             em.getTransaction().commit();
@@ -79,10 +70,8 @@ public class CountryDAO implements IDAO<Country>
     }
 
     @Override
-    public void delete(Long id)
-    {
-        try (EntityManager em = emf.createEntityManager())
-        {
+    public void delete(Long id) {
+        try (EntityManager em = emf.createEntityManager()) {
             em.getTransaction().begin();
             Country country = em.find(Country.class, id);
             em.remove(country);
@@ -90,17 +79,57 @@ public class CountryDAO implements IDAO<Country>
         }
     }
 
-    public void saveCountriesToDb(List<CountryDTO> countryDtoList)
-    {
-        try (EntityManager em = emf.createEntityManager())
-        {
+    public void saveCountriesToDb(List<CountryDTO> countryDtoList) {
+        try (EntityManager em = emf.createEntityManager()) {
             em.getTransaction().begin();
-            for (CountryDTO countryDTO : countryDtoList)
-            {
+            for (CountryDTO countryDTO : countryDtoList) {
                 Country country = new Country(countryDTO);
                 em.persist(country);
             }
             em.getTransaction().commit();
+        }
+    }
+
+    public void getCountryByName(String commonName) {
+        try (EntityManager em = emf.createEntityManager()) {
+            Country country = em.createQuery("SELECT c FROM Country c WHERE c.commonName = :commonname", Country.class)
+                    .setParameter("commonname", commonName)
+                    .getSingleResult();
+            System.out.println(country);
+        }
+    }
+
+    public List<Country> getCountriesByCurrency(String currencyName) {
+        try (EntityManager em = emf.createEntityManager()) {
+            return em.createQuery("SELECT c FROM Country c WHERE c.currencyName = :currencyname", Country.class)
+                    .setParameter("currencyname", currencyName)
+                    .getResultList();
+        }
+    }
+
+    public List<Country> getCountriesByLanguage(String language) {
+        try (EntityManager em = emf.createEntityManager()) {
+            return em.createQuery(
+                            "SELECT c FROM Country c JOIN c.languages lang WHERE lang = :language_name", Country.class)
+                    .setParameter("language_name", language)
+                    .getResultList();
+        }
+    }
+
+    public List<Country> getCountriesByLanguageAndIgnoreOtherLanguages(String language) {
+        try (EntityManager em = emf.createEntityManager()) {
+            return em.createQuery(
+                            "SELECT c FROM Country c JOIN c.languages lang WHERE lang = :language_name AND SIZE(c.languages) = 1", Country.class)
+                    .setParameter("language_name", "English")
+                    .getResultList();
+        }
+    }
+
+    public List<Country> getCountriesByCapital(String capital) {
+        try (EntityManager em = emf.createEntityManager()) {
+            return em.createQuery("SELECT c FROM Country c WHERE :capitals MEMBER OF c.capitals", Country.class)
+                    .setParameter("capitals", capital)
+                    .getResultList();
         }
     }
 }
