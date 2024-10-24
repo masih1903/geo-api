ecd748861843155ab23cf6bd28e013ee75afd165
MASIH1903 <144231041+MASIH1903@users.noreply.github.com>
2024-10-24 23:05:24 +0200

    Oprettet Fat jar fil og ændret port fra 7070 til 7007 i Dockerfile, Apiprops og har ændret rettigheder fra admin til anyone i countryRoute da denne projekt skal i gennem workflow og bestå testene.

--- src/main/java/app/utils/ApiProps.java
+++ src/main/java/app/utils/ApiProps.java
@@ -9,6 +9,6 @@ public class ApiProps {
     public static final String DB_URL = "jdbc:postgresql://localhost:5432/" + DB_NAME;
 
     // == API CONFIG ==
-    public static final int PORT = 7070;
+    public static final int PORT = 7007;
     public static final String API_CONTEXT = "/api";
 }
