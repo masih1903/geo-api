57217e63e2627e139b71ed957f76bb76b149ca9a
Umair-230104 <cph-ut38@cphbusiness.dk>
2024-12-08 17:58:52 +0100

    tilf'øjet cors

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -56,12 +56,14 @@ public class AppConfig {
 
     public static Javalin startServer() {
         var app = io.javalin.Javalin.create(AppConfig::configuration);
+
+        exceptionContext(app);
+        app.beforeMatched(accessController::accessHandler);
+
         // Add CORS support
         app.before(AppConfig::corsHeaders);
         app.options("/*", AppConfig::corsHeadersOptions);
 
-        exceptionContext(app);
-        app.beforeMatched(accessController::accessHandler);
         app.error(404, ctx -> ctx.json("Welcome to our GEO API - See the different routes at /routes"));
         app.start(ApiProps.PORT);
         return app;
